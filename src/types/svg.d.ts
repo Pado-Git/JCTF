01b1b77b76ba24a7cf298474c5b640bfa49841fb
heeso <h2s0_@naver.com>
2025-09-04 17:40:41 +0900

    🎨 image 파일 추가, icon 색깔 변경 가능하게 수정

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,5 +1,11 @@
 declare module '*.svg' {
   import React from 'react';
-  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  const SVG: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare module '*.svg?react' {
+  import React from 'react';
+  const SVG: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default SVG;
 }
