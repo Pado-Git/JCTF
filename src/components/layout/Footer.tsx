79a16e9bf0ada1809aa17f309d21fd3696f72fde
heeso <h2s0_@naver.com>
2025-09-05 13:55:41 +0900

    ✨ Footer 컴포넌트 추가

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return(
+    <div className="py-20 bg-neutral-900 text-center flex flex-col items-center justify-center gap-2">
+      <p className="text-heading-medium text-primary">JCTP</p>
+      <p className="text-body-small text-primary-200">The ultimate platform for cybersecurity competitions</p>
+      <p className="text-body-xsmall text-neutral-300 mt-4">© 2024 SURF-LAB JCTF. All rights reserved.</p>
+    </div>
+  )
+};
