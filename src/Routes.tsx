fd14d7cfc84ee449606259bb12ae02c28f644193
heesae <h2s0_@naver.com>
2025-09-19 16:45:15 +0900

    ✨ 📦 LINK.ts 에 라우트 경로 상수화 및 Competition 상세 경로 ( :compeitionId ) 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -13,19 +13,20 @@ import { LeaderboardPage } from '@/pages/Leaderboard';
 import { ProfilePage } from '@/pages/ProfilePage';
 import { TeamsPage } from '@/pages/TeamsPage';
 import { RootLayout } from '@/+shared/components';
+import { LINKS } from '@/+shared/constants';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout />}>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/challenges" element={<ChallengesPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/competitions" element={<Competitions />} />
-      <Route path="/teams" element={<TeamsPage />} />
-      <Route path="/leaderboard" element={<LeaderboardPage />} />
+      <Route path={LINKS.home} element={<HomePage />} />
+      <Route path={LINKS.login} element={<LoginPage />} />
+      <Route path={LINKS.register} element={<RegisterPage />} />
+      <Route path={LINKS.profile} element={<ProfilePage />} />
+      <Route path={LINKS.challenges} element={<ChallengesPage />} />
+      <Route path={LINKS.dashboard} element={<DashboardPage />} />
+      <Route path={LINKS.competitions} element={<Competitions />} />
+      <Route path={LINKS.teams} element={<TeamsPage />} />
+      <Route path={LINKS.leaderboard} element={<LeaderboardPage />} />
     </Route>
   ),
   { basename: '/' }
