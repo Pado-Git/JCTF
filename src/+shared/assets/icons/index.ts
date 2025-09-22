28e8972019fcaad948982b3408f370a4880cbfb8
heesae <h2s0_@naver.com>
2025-09-22 12:49:49 +0900

    🎨

--- src/+shared/assets/icons/index.ts
+++ src/+shared/assets/icons/index.ts
@@ -52,3 +52,5 @@ export { default as IcoUnlockFilled } from './ico_unlock-filled.svg?react';
 export { default as IcoUnlockLined } from './ico_unlock-lined.svg?react';
 export { default as IcoKeyLined } from './ico_key-lined.svg?react';
 export { default as IcoNotificationLined } from './ico_notification-lined.svg?react';
+export { default as IcoMailLined } from './ico_mail-lined.svg?react';
+export { default as IcoExitFilled } from './ico_exit-filled.svg?react';
