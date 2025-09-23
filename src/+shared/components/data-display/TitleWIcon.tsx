63f9ee49d0e34171b0ee3953c39001b364dc037a
heesae <h2s0_@naver.com>
2025-09-23 14:24:43 +0900

    🌊 TitleWIcon 컴포넌트 수정

--- src/+shared/components/data-display/TitleWIcon.tsx
+++ src/+shared/components/data-display/TitleWIcon.tsx
@@ -1,8 +1,8 @@
-export function TitleWIcon({ title, icon, description }: { title: string, icon: React.ReactNode, description?: string }) {
+export function TitleWIcon({ title, icon, description }: { title: string, icon?: React.ReactNode, description?: string }) {
   return(
     <div className="flex flex-col gap-2">
       <div className="flex items-center gap-4">
-        <span className="w-6 h-6 flex items-center justify-center text-primary">{icon}</span>
+        {icon && <span className="w-6 h-6 flex items-center justify-center text-primary">{icon}</span>}
         <span className="typo-heading-medium text-primary-200">{title}</span>
       </div>
       {description && (
