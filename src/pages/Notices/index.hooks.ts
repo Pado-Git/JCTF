8123df31e1f9ffc5a39bccecbde251bc3392f8b6
heesae <h2s0_@naver.com>
2025-10-15 15:22:28 +0900

    ✨ 공지사항 페이지 UI 구현

---
+++ src/pages/Notices/index.hooks.ts
@@ -0,0 +1,31 @@
+
+
+export function useNoticesPage() {
+  const notices = [
+    {
+      id: 1,
+      title: '2025 CTF Tournament Schedule Notice',
+      description: 'Corporate Security CThe schedule for the 2025 CTF Tournament has been confirmed. Preliminary Round: November 1 – November 3, 2025 Final Round: November 20 – November 22, 2025 Participant registration is open until October 25, 2025. Each team must consist of a minimum of 1 and a maximum of 4 members. For more details, please refer to the Tournament Rules page.',
+      author: 'Admin',
+      date: '2025-01-01',
+    },
+    {
+      id: 2,
+      title: '2025 CTF Tournament Schedule Notice 2025 CTF Tournament Schedule Notice 2025 CTF Tournament Schedule Notice',
+      description: 'Corporate Security CThe schedule for the 2025 CTF Tournament has been confirmed. Preliminary Round: November 1 – November 3, 2025 Final Round: November 20 – November 22, 2025 Participant registration is open until October 25, 2025. Each team must consist of a minimum of 1 and a maximum of 4 members. For more details, please refer to the Tournament Rules page.',
+      author: 'Admin',
+      date: '2025-01-01',
+    },
+    {
+      id: 3,
+      title: 'Notice 1',
+      description: 'Corporate Security CThe schedule for the 2025 CTF Tournament has been confirmed. Preliminary Round: November 1 – November 3, 2025 Final Round: November 20 – November 22, 2025 Participant registration is open until October 25, 2025. Each team must consist of a minimum of 1 and a maximum of 4 members. For more details, please refer to the Tournament Rules page.',
+      author: 'Admin',
+      date: '2025-01-01',
+    },
+  ];
+
+  return {
+    notices
+  }
+}
--- src/pages/Notices/index.tsx
+++ src/pages/Notices/index.tsx
@@ -1,7 +1,44 @@
-
+import { IcoPinFilled, MaxWidthContainer } from "@/+shared";
+import { IcoCalendarLined } from '@/+shared/assets';
+import { IcoIndividualLined } from '@/+shared/assets/icons';
+import { useNoticesPage } from "./index.hooks";
 
 export function NoticesPage() {
+  const { notices } = useNoticesPage();
 
+  return (
+    <>
+      <section className="flex flex-col gap-2 py-16 gradient-3-deg">
+        <MaxWidthContainer>
+          <span className="typo-heading-large text-primary-50">Notice</span>
+          <span className="typo-body-medium text-primary-100">Check out the latest updates and important announcements from the CTF platform.</span>
+        </MaxWidthContainer>
+      </section>
+      <MaxWidthContainer>
+        <section className="flex flex-col gap-4">
+          {notices.map((notice, index) => (
+            <div key={index} className="flex flex-col gap-4 p-8 bg-neutral-800 border border-neutral-500 rounded-radius-md">
+              <div className="flex items-center gap-4">
+                <IcoPinFilled className="w-5 h-5 text-primary" />
+                <div className="flex items-center gap-1 text-neutral-100 typo-body-xsmall">
+                  <IcoCalendarLined className="w-4 h-4" />
+                  <span>{notice.date}</span>
+                </div>
+                <div className="flex items-center gap-1 text-neutral-100 typo-body-xsmall">
+                  <IcoIndividualLined className="w-4 h-4" />
+                  <span>{notice.author}</span>
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h2 className="typo-heading-small">{notice.title}</h2>
+                <span className="text-neutral-100 typo-body-small">{notice.description}</span>
+              </div>
+            </div>
+          ))}
+        </section>
+      </MaxWidthContainer>
+    </>
+  )
 }
 
 export default NoticesPage;
