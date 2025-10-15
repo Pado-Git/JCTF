d367d7b1f5703411061b93a3a91c7f72853421ad
heesae <h2s0_@naver.com>
2025-10-15 11:24:24 +0900

    🐛

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwindcss(),
     svgr()
   ],
-  base: import.meta.env.VITE_BASE_URL || '/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
